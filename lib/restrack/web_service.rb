033dfd12a9f5b02fb13d1df72710aebe6ccd6e3d
Chris St. John <chris@stjohnstudios.com>
2012-09-26 16:54:03 -0400

    pre- and post-processing hooks support and tests

--- lib/restrack/web_service.rb
+++ lib/restrack/web_service.rb
@@ -4,14 +4,15 @@ module RESTRack
     # Establish the namespace pointer.
     def initialize
       RESTRack::CONFIG[:SERVICE_NAME] = self.class.to_s.split('::')[0].to_sym
+      @request_hook = RESTRack::Hooks.new
     end
 
     # Handle requests in the Rack way.
     def call( env )
       resource_request = RESTRack::ResourceRequest.new( :request => Rack::Request.new(env) )
-      RESTRack::Hooks.pre_processor(resource_request) unless RESTRack::CONFIG.has_key?(:PRE_PROCESSOR_DISABLED) and RESTRack::CONFIG[:PRE_PROCESSOR_DISABLED]
+      @request_hook.pre_processor(resource_request) unless RESTRack::CONFIG.has_key?(:PRE_PROCESSOR_DISABLED) and RESTRack::CONFIG[:PRE_PROCESSOR_DISABLED]
       response = RESTRack::Response.new(resource_request)
-      RESTRack::Hooks.post_processor(resource_request) unless RESTRack::CONFIG.has_key?(:POST_PROCESSOR_DISABLED) and RESTRack::CONFIG[:POST_PROCESSOR_DISABLED]
+      @request_hook.post_processor(resource_request) unless RESTRack::CONFIG.has_key?(:POST_PROCESSOR_DISABLED) and RESTRack::CONFIG[:POST_PROCESSOR_DISABLED]
       return response.output
     end # method call
 
