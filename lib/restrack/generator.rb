90b0b15c3b4b4b8b67c490722f266ea71dfa15ff
Chris St. John <chris@stjohnstudios.com>
2012-09-26 11:33:12 -0400

    adding support for preprocessing and postprocessing hooks

--- lib/restrack/generator.rb
+++ lib/restrack/generator.rb
@@ -5,17 +5,18 @@ require 'active_support/inflector'
 
 module RESTRack
   class Generator
-    
+
     TEMPLATE = {
       :service                => 'loader.rb.erb',
       :rackup                 => 'config.ru.erb',
       :constants              => 'constants.yaml.erb',
       :controller             => 'controller.rb.erb',
-      :descendant_controller  => 'descendant_controller.rb.erb'
+      :descendant_controller  => 'descendant_controller.rb.erb',
+      :hooks                  => 'hooks.rb.erb'
     }
-    
+
     class << self
-      
+
       # Generate controller file
       def generate_controller(name)
         template = get_template_for( :controller )
@@ -24,7 +25,7 @@ module RESTRack
         # Generate view folder for controller
         FileUtils.makedirs("#{base_dir}/views/#{name}")
       end
-      
+
       # Generate controller file the descends from specified parent, to enable
       # grouping of controller types and/or overarching functionality.
       def generate_descendant_controller(name, parent)
@@ -34,7 +35,7 @@ module RESTRack
         # Generate view folder for controller
         FileUtils.makedirs("#{base_dir}/views/#{name}")
       end
-  
+
       # Generate a new RESTRack service
       def generate_service(name)
         FileUtils.makedirs("#{name}/config")
@@ -42,45 +43,49 @@ module RESTRack
         FileUtils.makedirs("#{name}/models")
         FileUtils.makedirs("#{name}/test")
         FileUtils.makedirs("#{name}/views")
-        
+
         template = get_template_for( :service )
         resultant_string = template.result( get_binding_for_service( name ) )
         File.open("#{name}/loader.rb", 'w') {|f| f.puts resultant_string }
-        
+
         template = get_template_for( :rackup )
         resultant_string = template.result( get_binding_for_service( name ) )
         File.open("#{name}/config.ru", 'w') {|f| f.puts resultant_string }
-        
+
         template = get_template_for( :constants )
         resultant_string = template.result( get_binding_for_service( name ) )
         File.open("#{name}/config/constants.yaml", 'w') {|f| f.puts resultant_string }
+
+        template = get_template_for( :hooks )
+        resultant_string = template.result( get_binding_for_service( name ) )
+        File.open("#{name}/hooks.rb", 'w') {|f| f.puts resultant_string }
       end
-  
+
       private
-  
+
       def get_template_for(type)
         template_file = File.new(File.join(File.dirname(__FILE__),"generator/#{TEMPLATE[type]}"))
         template = ERB.new( template_file.read, nil, "%" )
       end
-  
+
       def get_binding_for_controller(name)
         @name = name
         @service_name = get_service_name
         binding
       end
-      
+
       def get_binding_for_descendant_controller(name, parent)
         @name = name
         @parent = parent
         @service_name = get_service_name
         binding
       end
-  
+
       def get_binding_for_service(name)
         @service_name = name
         binding
       end
-  
+
       def get_service_name
         line = ''
         begin
@@ -96,7 +101,7 @@ module RESTRack
         end
         return service_name
       end
-  
+
       def base_dir
         base_dir = nil
         this_path = File.join( Dir.pwd, 'config/constants.yaml')
@@ -111,7 +116,7 @@ module RESTRack
         raise 'The config/constants.yaml file could not found when determining base_dir!' unless base_dir
         return base_dir
       end
-    
+
     end # class << self
 
   end # class
--- lib/restrack/web_service.rb
+++ lib/restrack/web_service.rb
@@ -9,7 +9,9 @@ module RESTRack
     # Handle requests in the Rack way.
     def call( env )
       resource_request = RESTRack::ResourceRequest.new( :request => Rack::Request.new(env) )
+      RESTRack::Hooks.pre_processor(resource_request) unless RESTRack::CONFIG.has_key?(:PRE_PROCESSOR_DISABLED) and RESTRack::CONFIG[:PRE_PROCESSOR_DISABLED]
       response = RESTRack::Response.new(resource_request)
+      RESTRack::Hooks.post_processor(resource_request) unless RESTRack::CONFIG.has_key?(:POST_PROCESSOR_DISABLED) and RESTRack::CONFIG[:POST_PROCESSOR_DISABLED]
       return response.output
     end # method call
 
